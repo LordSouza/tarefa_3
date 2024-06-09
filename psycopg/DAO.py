8cc31e343633d444c945dc8580dc554b8f30066b
Lucas Oliveira <d2022004860@unifei.edu.br>
2024-06-09 17:47:50 -0300

    format

--- psycopg/DAO.py
+++ psycopg/DAO.py
@@ -42,7 +42,9 @@ def search_customers(name: str):
     # buscar um cliente
     try:
         db = next(get_db())
-        return db.execute("SELECT * FROM northwind.customers WHERE contactname LIKE %s", (name,)).fetchall()
+        return db.execute(
+            "SELECT * FROM northwind.customers WHERE contactname LIKE %s", (name,)
+        ).fetchall()
     except Exception as e:
         print(e)
 
@@ -51,7 +53,9 @@ def search_products(productname: str):
     # buscar um produto
     try:
         db = next(get_db())
-        return db.execute("SELECT * FROM northwind.products WHERE productname LIKE %s", (productname,)).fetchall()
+        return db.execute(
+            "SELECT * FROM northwind.products WHERE productname LIKE %s", (productname,)
+        ).fetchall()
     except Exception as e:
         print(e)
 
@@ -60,7 +64,10 @@ def search_employee(name: str):
     # inserir um novo funcionário
     try:
         db = next(get_db())
-        return db.execute("SELECT * FROM northwind.employees WHERE lastname LIKE %s OR firstname LIKE %s", (name,name)).fetchall()
+        return db.execute(
+            "SELECT * FROM northwind.employees WHERE lastname LIKE %s OR firstname LIKE %s",
+            (name, name),
+        ).fetchall()
     except Exception as e:
         print(e)
 
@@ -69,7 +76,9 @@ def search_employee_by_id(id: int):
     # inserir um novo funcionário
     try:
         db = next(get_db())
-        return db.execute("SELECT * FROM northwind.employees WHERE employeeid = %s", (id,)).fetchone()
+        return db.execute(
+            "SELECT * FROM northwind.employees WHERE employeeid = %s", (id,)
+        ).fetchone()
     except Exception as e:
         print(e)
 
@@ -78,33 +87,45 @@ def search_order_by_id(order_id: int):
     # buscar um pedido pelo id
     try:
         db = next(get_db())
-        return db.execute("SELECT * FROM northwind.orders WHERE orderid = %s", (order_id,)).fetchone()
+        return db.execute(
+            "SELECT * FROM northwind.orders WHERE orderid = %s", (order_id,)
+        ).fetchone()
     except Exception as e:
         print(e)
+
 
 def search_products_by_id(product_id: int):
     # buscar um produto pelo id
     try:
         db = next(get_db())
-        return db.execute("SELECT * FROM northwind.products WHERE productid = %s", (product_id,)).fetchone()
+        return db.execute(
+            "SELECT * FROM northwind.products WHERE productid = %s", (product_id,)
+        ).fetchone()
     except Exception as e:
         print(e)
+
 
 def search_customer_by_id(customer_id: str):
     # buscar um cliente pelo id
     try:
         db = next(get_db())
-        return db.execute("SELECT * FROM northwind.customers WHERE customerid = %s", (customer_id,)).fetchone()
+        return db.execute(
+            "SELECT * FROM northwind.customers WHERE customerid = %s", (customer_id,)
+        ).fetchone()
     except Exception as e:
         print(e)
+
 
 def select_order_details_by_order_id(order_id: int):
     # buscar os detalhes de um pedido
     try:
         db = next(get_db())
-        return db.execute("SELECT * FROM northwind.order_details WHERE orderid = %s", (order_id,)).fetchall()
+        return db.execute(
+            "SELECT * FROM northwind.order_details WHERE orderid = %s", (order_id,)
+        ).fetchall()
     except Exception as e:
         print(e)
+
 
 def select_count_orders_by_employee(
     data_inicial: datetime.datetime, data_final: datetime.datetime
@@ -112,12 +133,14 @@ def select_count_orders_by_employee(
     # buscar todos os funcionários
     try:
         db = next(get_db())
-        return db.execute("""select o.employeeid, count(o.employeeid), sum(od.unitprice * od.quantity)
+        return db.execute(
+            """select o.employeeid, count(o.employeeid), sum(od.unitprice * od.quantity)
                                 from northwind.orders o
                                 left join northwind.order_details od 
                                 on od.orderid = o.orderid 
                                 where o.orderdate between %s and %s 
-                                group by o.employeeid""", 
-                                (data_inicial, data_final)).fetchall()
+                                group by o.employeeid""",
+            (data_inicial, data_final),
+        ).fetchall()
     except Exception as e:
         print(e)
